ed7fa0e9f9eb51838776f11196f9666d6648101d
ZehnVorRolexX <BBendler03@gmx.de>
2021-10-21 20:48:29 +0200

    Richtige Pfade gesetzt

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from flask import Flask
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True, host = "0.0.0.0", ssl_context = ('D:\\Dev\\Website\\website\\certs\\cert.pem', 'D:\\Dev\\Website\\website\\certs\\key.pem'))
+    app.run(debug = True, host = "0.0.0.0", ssl_context = ('/home/Basti/Website/Website/website/certs/haarstudio-mb.de_ssl_certificate.cer', '/home/Basti/Website/Website/website/certs/haarstudio-mb.de_private_key.key'), port='443')
