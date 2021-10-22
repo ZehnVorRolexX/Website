4b4a7a388b9f947dfd1e8f6aed795481ad053bfe
ZehnVorRolexX <BBendler03@gmx.de>
2021-10-22 16:57:42 +0200

    neue Certs hinzugefügt, slideshow auf Startseite

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from flask import Flask
 app = create_app()
 
 if __name__ == '__main__':
+    #before commiting, comment the app.run(debug = True) and uncomment the line above
     app.run(debug = True, host = "0.0.0.0", ssl_context = ('/home/Basti/Website/Website/website/certs/haarstudio-mb.de_ssl_certificate.cer', '/home/Basti/Website/Website/website/certs/haarstudio-mb.de_private_key.key'), port='443')
+    #app.run(debug = True)
